3a53adce8098da0d897617146a97a7fa700a0589
Paul Davis <pjdavis@gmx.com>
2020-01-28 00:17:32 +0000

    Add unique together constraint time siteid

--- airgql/models/aurn_hourly.py
+++ airgql/models/aurn_hourly.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, DateTime, Integer, ForeignKey
-
+from sqlalchemy import Column, DateTime, Integer, ForeignKey, UniqueConstraint
 from .meta import Base
 
 
 class AurnHourly(Base):
     __tablename__ = 'aurn_hourly'
+    __table_args__ = (
+        UniqueConstraint('time', 'site_id', name='uix_1'),
+    )
     id = Column(Integer, primary_key=True)
     time = Column(DateTime, nullable=False)
     site_id = Column(ForeignKey('aurn_sites.id'), nullable=False)
